789dad91d589f3d1ac7ed8669feac661fd927510
Rheina <test@Rheinas-Mac-mini.local>
2024-11-07 10:26:16 -0500

    change @/components to ../components to match other imports

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import ArrowDown from '../assets/icons/arrow-down.svg'
 import grainImage from '../assets/images/grain.jpg'
 import StarIcon from '../assets/icons/star.svg'
 import SparkleIcon from '../assets/icons/sparkle.svg'
-import { HeroOrbit } from '@/components/HeroOrbit'
+import { HeroOrbit } from '../components/HeroOrbit'
 export const HeroSection = () => {
   return( 
   <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">

b90496fee7bc237a4d5756ff96b3e80bfa148f2f
Rheina <test@Rheinas-Mac-mini.local>
2024-11-08 11:37:09 -0500

    Add testimonials page, section header component and card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import {Header} from "../sections/Header"
 import {HeroSection} from "../sections/Hero"
 import {ProjectsSection} from "../sections/Projects"
-
+import { TestimonialsSection } from "../sections/Testimonials";
 export default function Home() {
   return (
     <div>
       <Header/>
       <HeroSection/>
       <ProjectsSection/>
+      <TestimonialsSection/>
     </div>
   );
 }
